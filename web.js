cfed10c362449c98a76385764031f88f17dbce9a
Ubuntu <ubuntu@ip-172-31-33-159.us-west-2.compute.internal>
2013-07-11 22:01:21 +0000

    Edit web.js file

--- web.js
+++ web.js
@@ -3,10 +3,15 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 var file = 'index.html';
+
+//Returns a buffer
 var response = fs.readFileSync(file);
 
+//Convert buffer to string
+var string = response.toString('utf-8');
+
 app.get('/', function(request, response) {
-  response.send(response);
+  response.send(string);
 });
 
 var port = process.env.PORT || 5000;
